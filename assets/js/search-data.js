b73d5ee7b1c9a9d8e3350d1153b0d8176b798557
arnoblalam <arnoblalam@gmail.com>
2025-09-14 00:49:51 +0000

    Deploying to gh-pages from @ arnoblalam/arnoblalam.github.io@635b40058b03fa421790baec57a0395ca167c271 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+        },{
         id: 'social-github',
         title: 'GitHub',
         section: 'Socials',
